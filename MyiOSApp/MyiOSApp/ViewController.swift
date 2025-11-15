a5ff7413eda53db385d7c02ce9646004e0dab73f
JIN <jin724596@gmail.com>
2025-11-16 00:37:00 +0900

    테스트 코드 실습 과제 해보기

---
+++ MyiOSApp/MyiOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyiOSApp
+//
+//  Created by JIN on 11/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
