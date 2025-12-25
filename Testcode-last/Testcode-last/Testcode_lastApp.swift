17315d868031b64bb928ce834dfcfaa96c551cb4
JIN <jin724596@gmail.com>
2025-12-25 21:11:22 +0900

    프로젝트 셋업

---
+++ Testcode-last/Testcode-last/Testcode_lastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testcode_lastApp.swift
+//  Testcode-last
+//
+//  Created by JIN on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Testcode_lastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
