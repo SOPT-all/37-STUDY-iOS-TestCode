2d3bad09798fe5d0c222e13392b60d6083915141
Yeonnies <eliyeon16@gmail.com>
2025-12-26 23:37:24 +0900

    [test] 테스트코드 작성

--- CalculatorTest/CalculatorTestTests/CalculatorTestTests.swift
+++ CalculatorTest/CalculatorTestTests/CalculatorTestTests.swift
@@ -10,27 +10,81 @@ import XCTest
 
 final class CalculatorTestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var sut: CalculatorModel!
+    
+    override func setUp() {
+        super.setUp()
+        sut = CalculatorModel()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    // MARK: - Addition Tests
+    
+    func test_performOperation_AddTwoNumbers_ReturnsSum() {
+        // Given: 5 + 3을 입력
+        sut.inputNumber("5")
+        sut.inputOperation(.add)
+        sut.inputNumber("3")
+        
+        // When: = 버튼을 누름
+        sut.performOperation()
+        
+        // Then: 결과는 8
+        XCTAssertEqual(sut.displayValue, "8")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    
+    // MARK: - Subtraction Tests
+    
+    func test_performOperation_SubtractTwoNumbers_ReturnsDifference() {
+        // Given: 10 - 3을 입력
+        sut.inputNumber("1")
+        sut.inputNumber("0")
+        sut.inputOperation(.subtract)
+        sut.inputNumber("3")
+        
+        // When: = 버튼을 누름
+        sut.performOperation()
+        
+        // Then: 결과는 7
+        XCTAssertEqual(sut.displayValue, "7")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    
+    // MARK: - Multiplication Tests
+    
+    func test_performOperation_MultiplyTwoNumbers_ReturnsProduct() {
+        // Given: 4 × 5를 입력
+        sut.inputNumber("4")
+        sut.inputOperation(.multiply)
+        sut.inputNumber("5")
+        
+        // When: = 버튼을 누름
+        sut.performOperation()
+        
+        // Then: 결과는 20
+        XCTAssertEqual(sut.displayValue, "20")
     }
-
+    
+    
+    // MARK: - Division Tests
+    
+    func test_performOperation_DivideTwoNumbers_ReturnsQuotient() {
+        // Given: 15 ÷ 3을 입력
+        sut.inputNumber("1")
+        sut.inputNumber("5")
+        sut.inputOperation(.divide)
+        sut.inputNumber("3")
+        
+        // When: = 버튼을 누름
+        sut.performOperation()
+        
+        // Then: 결과는 5
+        XCTAssertEqual(sut.displayValue, "5")
+    }
 }
